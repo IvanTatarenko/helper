bf5bc6cb4d552295b610b7b9e3d3618cc77a706b
Vakum <vakummail@gmail.com>
2024-01-13 10:37:29 +0200

    fix

--- src/image_for_dataset.py
+++ src/image_for_dataset.py
@@ -2,7 +2,7 @@ from module.folder.folder_service import FolderService
 from module.logger.logger_service import LoggerService
 
 logger = LoggerService('ImageForDatasetFile')
-logger.success('Запуск підготовки зображень для датасуту')
+logger.success('Запуск підготовки зображень для датасету')
 
 folder_service = FolderService()
 folder_service.create_base_folders()
