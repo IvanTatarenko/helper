f913f54b682565f6645f44e9a894e70d94702412
Vakum <vakummail@gmail.com>
2024-01-13 11:19:37 +0200

    add exeption for folder service

--- src/services/folder/folder_service.py
+++ src/services/folder/folder_service.py
@@ -9,27 +9,43 @@ class FolderService:
 
   def __exists_folder(self, path):
     """Перевірка чи існує папка"""
-    if not os.path.exists(path):
-      self.logger.info(f"Папка не існує: {path}")
-      return False
-    else:
-      self.logger.info(f"Папка вже існує: {path}")
-      return True
+    try:
+      if not os.path.exists(path):
+        self.logger.info(f"Папка не існує: {path}")
+        return False
+      else:
+        self.logger.info(f"Папка вже існує: {path}")
+        return True
+    except Exception as err:
+      self.logger.err('Помилка перевірки існування папки:')
+      self.logger.err(err)
 
   def __create_folder(self, path):
     """Створення папки якщо вона не існує"""
-    if not self.__exists_folder(path):
-      os.makedirs(path)
-      self.logger.success(f"Папка створена: {path}")
+    try:
+      if not self.__exists_folder(path):
+        os.makedirs(path)
+        self.logger.success(f"Папка створена: {path}")
+    except Exception as err:
+      self.logger.err('Помилка створення папки:')
+      self.logger.err(err)
 
   def create_folder_for_export(self, folder_name):
     """Створення папки в папці export"""
-    full_path = os.path.join(self.EXPORT_PATH, folder_name)
+    try:
+      full_path = os.path.join(self.EXPORT_PATH, folder_name)
+    except Exception as err:
+      self.logger.err('Помилка комбінування шляху до папки експорту:')
+      self.logger.err(err)
     self.__create_folder(full_path)
 
   def create_folder_for_import(self, folder_name):
     """Створення папки в папці import"""
-    full_path = os.path.join(self.IMPORT_PATH, folder_name)
+    try:
+      full_path = os.path.join(self.IMPORT_PATH, folder_name)
+    except Exception as err:
+      self.logger.err('Помилка комбінування шляху до папки імпорту:')
+      self.logger.err(err)
     self.__create_folder(full_path)
 
   def create_base_folders(self):
